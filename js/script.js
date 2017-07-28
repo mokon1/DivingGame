23d48daf6de823647e70fedc6f47af6537b18279
mokon1 <michalina.okon@gmail.com>
2017-07-28 14:12:52 +0200

    updated music,coins,ordered code

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
+//scipt jest ok //zmiana kolejności/zmiana music//dodanie obiektow//addCoin-dodanie//
 $(function(){
     
     //MUSIC
-    var audio = $("audio")[0];
-    var startSong = $(".music")[0];
-    var oceanSong = $(".ocean")[0];
-    var coinsSong = $(".coins")[0];
-    var endSong = $(".end")[0];
+    var audio = $("audio");
+    var startSong = $("#startSong")[0];
+    var oceanSong = $("#oceanSong")[0];
+    var coinsSong = $("#coinsSong")[0];
+    var endSong = $("#endSong")[0];
     startSong.play();
      
     //BUTTONS
@@ -15,85 +16,61 @@ $(function(){
     var musicBtn = $(".musicBtn");
     var tableScoresBtn = $(".tableScoresBtn");
     
-    //SCREEN
+    //SCREENS
+    var screens =$(".screen");
     var startScreen = $("#startScreen");
     var gameScreen = $("#gameScreen");
-    var instructionScreen = $("#instructionScreen");
-    var endScreen = $("#endScreen");
-    var information = $("#information");
-    
-    startBtn.on("click", function(event){
-       event.preventDefault();
-        startNewGame();
-        startScreen.fadeOut(50); //nie zmieniać czasu, bo ptk nie będą się naliczać/złoto ukrywać
-        gameScreen.fadeIn(100);
-        information.fadeIn(100);
-        startSong.pause();
-        oceanSong.play();     
-    });
-    
-    instructionBtn.on("click",function(event){
-        event.preventDefault();
-        startScreen.fadeOut(1000);
-        instructionScreen.delay(1000).fadeIn(1000);
-        startSong.pause();
-        endSong.play();
-    });
-    
-    musicBtn.on("click",function(event){
-        event.preventDefault();
-        var textBtn = $(this).find('p');        
-        
-        if(textBtn.text()==="SOUND ON"){
-            textBtn.text("SOUND OFF");
-            startSong.play();
-        }else{
-            textBtn.text("SOUND ON");
-            startSong.pause();
-        }
-        
-    });
-    var form = $("form");
-        
-    tableScoresBtn.on("click",function(event){
-        event.preventDefault();
-        startScreen.fadeOut(1000);
-        endScreen.delay(1000).fadeIn(1000);
-       // form.addClass('invisible'); ///do skończenia - koniec gry usunąć klase invisible
-    });
-    
-    endGameBtn.on("click", function(event){
-        event.preventDefault();
-        backToMenu();
-    });
-    
-    
-    //ITEMS
-    var time = $('#time');
-    var character = $('.character');
-    var notice = $('#result');
-    var timeCounter = 15;
     var heightScreen = gameScreen.height();
     var widthScreen = gameScreen.width();
-    var heightCharacter = character.height();
-    var widthCharacter = character.width();
+    var instructionScreen = $("#instructionScreen");
+    var endScreen = $("#endScreen");
     
-    // START NEW GAME !!!
+    //ELEMENTS THAT SHOW INFORMATION
+    var information = $("#information");
+    var form = $("#endScreen").find("form");
+    var endScore = $("#endScreen").find(".endScore");
+    var notice = $('#result');
+    var showResult = $(".showResult");
+    var showResult2 = $(".showResult2");
+    var time = $('#time');
+    
+    //SETTINGS
+    var timeCounter = 45;
+    var collectedCoins = 0;
+    var score = 0;
+    var minDistance = 100; 
+    var maxDistanceTop = $('#gameScreen').height()-100;     //coinsy muszą spadać
+    var maxDistanceLeft = $('#gameScreen').width()+400;     //coinsy musza sie ruszac
+    var randomX = Math.random() * (widthScreen-200)+100;
+    var randomY = Math.random() * (heightScreen-400)+200; 
+    
+    //ELEMENTS IN GAME
+    var diver = $('.diver');
+    var heightDiver = diver.height();
+    var widthDiver = diver.width();
+    var coins = $('.item');
+    var silverCoins = $(".silverCoin");
+    var goldCoins = $(".goldCoin");
+    var danger = $(".danger");
+   //var elements = $(".item"); do usunięcia
+    //var fishBlack = $(".fishBlack1");
+    
+        
+    // START NEW GAME
     function startNewGame(){
         score = 0;
-        timeCounter = 15; //zmienić na 90
+        timeCounter = 45;
         startPosition();
         functionSetInterval();
-    }    
-     
-       /* TIME COUNTER    //ok*/
+    }
+
+            
+       /* TIME COUNTER */
     function functionSetInterval(){        
         
         var interval = setInterval(function(){
             
-            console.log(character.position().left);
-        console.log(widthScreen);
-            
+            //timeCounter
             timeCounter--;
             if(timeCounter === 10){
                 time.addClass('finalCounting');
@@ -102,155 +79,213 @@ $(function(){
                 clearInterval(interval);
                 endGame();
             }
-            
             time.text(timeCounter);
-            //$('#result').text(score);
+            $('#result').text(score);
             
+            //collect coins&dangerous
             $(".item").each(function(){
                 if (
-                    (character.position().top + character.outerHeight() >=  $(this).position().top)
+                    (diver.position().top + diver.outerHeight() >=  $(this).position().top)
                         && 
-                    (character.position().top <= $(this).position().top+$(this).outerHeight())
+                    (diver.position().top <= $(this).position().top+$(this).outerHeight())
                         && 
-                    (character.position().left + character.outerWidth() >=  $(this).position().left)
+                    (diver.position().left + diver.outerWidth() >=  $(this).position().left)
                         && 
-                    (character.position().left <= $(this).position().left + $(this).outerWidth())
+                    (diver.position().left <= $(this).position().left + $(this).outerWidth())
                     ){  
-                    if ($(this).hasClass('silverCoin')){
-                        $(this).fadeOut(100);
-                        score++;
-                    } else if ($(this).hasClass('goldCoin')){
-                        $(this).fadeOut(100);
-                        score+=10;
-                    }else{
-                        endGame();
-                        clearInterval(interval);
-                    }    
+                        if ($(this).hasClass('silverCoin')){
+                            $(this).fadeOut(100);
+                            coinsSong.play();
+                            score++;
+                            collectedCoins++;
+                            addCoins();
+                        } else if ($(this).hasClass('goldCoin')){
+                            $(this).fadeOut(100);
+                            score+=10;
+                        }else{
+                           // endGame();
+                            //clearInterval(interval);
+                        }    
                     }
-            })
-           // console.log(character.position().bottom)
-            if (character.position().top < -50) {
-               character.animate({
+            });  
+            
+            //borders
+            if (diver.position().top < -50) {
+               diver.animate({
                     'top': '0' +'px'
                 });
             }
-            if (character.position().top > heightScreen) {
-               character.animate({
-                    'top': heightScreen-heightCharacter
+            if (diver.position().top > heightScreen) {
+               diver.animate({
+                    'top': heightScreen-heightDiver
                 });
             } 
-            if (character.position().left < -50) {
-               character.animate({
+            if (diver.position().left < -50) {
+               diver.animate({
                     'left': '0' +'px'
                 });
             } 
-            if (character.position().left + widthCharacter > widthScreen+50) {
-               character.animate({
+            if (diver.position().left + widthDiver > widthScreen-350) { //minus 350-sea current
+               diver.animate({
                     'left': '60vw'
                 });
             } 
         },1000);
     }
+    
+    function addCoins(){       
+        if (collectedCoins%3 == 0){
+            coins.each(function(){
+                if ($(this).css("display")==="none"){
+                    newLeft = ((Math.random() * (maxDistanceLeft-minDistance))+minDistance).toFixed();
+                    newTop = ((Math.random()*(maxDistanceTop-minDistance))+minDistance).toFixed();
+                    $(this).css({
+                        'left': newLeft+'px',
+                        'top': newTop+'px',
+                        'display': 'block'
+                    },500);
+                }
+            })     
+        }
+    } 
+    
  
-    function backToMenu(){
-        instructionScreen.fadeOut(1000);
-        gameScreen.fadeOut(1000);
-        information.fadeOut(1000);
-        endScreen.fadeOut(1000);
-        startScreen.delay(1000).fadeIn(1000);
-        endSong.pause();
-        time.removeClass('finalCounting');
-        ///oceanSong.pause();
-        //startSong.play();
-      // odNowa();
-    }
-    var showResult = $(".showResult");
-    var showResult2 = $(".showResult2");
     function endGame(){
-        gameScreen.fadeOut(1000);
-        information.fadeOut(1000);
+        screens.fadeOut(1000);
         endScreen.fadeIn(1100);
         oceanSong.pause();
         endSong.play();
         showResult.text(score);
-        showResult2.text(score);
+        showResult2.val(score);
     }
     
-    
-    /* COINS, CHARACTER POSITION */
-    var coins = $('.item');
-    var minDistance = 100; 
-    var maxDistanceTop = $('#gameScreen').height()-100;     //coinsy muszą spadać
-    var maxDistanceLeft = $('#gameScreen').width()+400;     //coinsy musza sie ruszac
-
-
+    /* NEW GAME */
     function startPosition() {
-        character.css('top','350px').css('left','10px');
-        coins.each(function(){
-            left2 = ((Math.random() * (maxDistanceLeft-minDistance))+minDistance).toFixed();
-            top2 = ((Math.random()*(maxDistanceTop-minDistance))+minDistance).toFixed();
-            //console.log($('#gameScreen').height());
-            $(this).css({
-                'left': left2+'px',
-                'top':  top2+'px',
-                'display': 'block'
-            },500);
+            goldCoins.each(function(){
+                left2 = ((Math.random() * (maxDistanceLeft-minDistance))+minDistance).toFixed();
+                top2 = ((Math.random()*(maxDistanceTop-minDistance))+minDistance).toFixed();
+                $(this).css({
+                    'left': left2+'px',
+                    'top':  top2+'px',
+                    'display': 'block'
+                },500);
 
-            if(left2>$('#gameScreen').width()-50){
-                $(this).addClass('moveToLeft');
-            }   
-            if(top2<200){
-                $(this).addClass('moveToBottom');
-            }
+                if(left2>$('#gameScreen').width()-50){
+                    $(this).addClass('moveToLeft');
+                }   
+                if(top2<200){
+                    $(this).addClass('moveToBottom');
+                }
+            });
+
+            silverCoins.each(function(){
+                left2 = ((Math.random() * (maxDistanceLeft-minDistance))+minDistance).toFixed();
+                top2 = ((Math.random()*(maxDistanceTop-minDistance))+minDistance).toFixed();
+                //console.log($('#gameScreen').height());
+                $(this).css({
+                    'left': left2+'px',
+                    'top':  top2+'px',
+                    'display': 'block'
+                },5000);
+
+                if(left2>$('#gameScreen').width()-50){
+                    $(this).addClass('moveToLeft');
+                }   
+                if(top2<200){
+                    $(this).addClass('moveToBottom');
+                }
         });
     }
     
   
     /* MOVEMENT */
-    var character = $('.character');
-    var score = 0;
-    var elements = $(".item");
-    
-
     $(document).keydown(function(direction) {
          notice.text(score);
           switch (direction.which){
                 case 37:{
-                    character.css({
+                    diver.css({
                         'transform': 'rotate(-180deg)',
                         'left': '-=10'  
                     })
                     break;
                 }
                 case 38:{
-                     character.css({
+                     diver.css({
                         'transform': 'rotate(-45deg)',
                         'top': '-=10'  
                     })
                     break;
                 }
                 case 39:{
-                     character.css({
+                     diver.css({
                         'transform': 'rotate(-0deg)',
                         'left': '+=10'  
                     })
                     break;
                 }   
                 case 40:{
-                     character.css({
+                     diver.css({
                         'transform': 'rotate(450deg)',
                         'top': '+=10'  
                     })
                     break;
                 }
               default:{
-                 notice.text("Poruszaj się strzałkami.");
+                 notice.text("Please, use the arrow keys to move the diver.");
               }
         }
     });
     
     
-        /* AJAX */
+    /* BUTTONS */
+    startBtn.on("click", function(event){
+        event.preventDefault(); //????
+        startNewGame();
+        startScreen.fadeOut(50); //nie zmieniać czasu, bo ptk nie będą się naliczać/złoto ukrywać
+        gameScreen.fadeIn(100);
+        information.fadeIn(100);
+        startSong.pause();
+        oceanSong.play();
+        endScore.removeClass('invisible'); //connected with SCORES SCREEN.
+        form.removeClass('invisible'); //connected with SCORES SCREEN.
+    });
+    
+    instructionBtn.on("click",function(event){
+        event.preventDefault();//????
+        startScreen.fadeOut(100);
+        instructionScreen.delay(1000).fadeIn(1000).css("display","flex");
+    });
+    
+    musicBtn.on("click",function(event){
+        //*event.preventDefault();//????          
+        if($(this).text() === "SOUND ON"){
+            $(this).text("SOUND OFF");
+            audio.prop("volume", 1);
+        }else{
+            $(this).text("SOUND ON"); 
+             audio.prop("volume", 0);
+        }
+    });
+          
+    tableScoresBtn.on("click",function(event){
+        event.preventDefault();
+        startScreen.fadeOut(1000);
+        endScreen.delay(1000).fadeIn(1000);
+        endScore.addClass('invisible');
+        form.addClass('invisible');
+    });
+    
+    endGameBtn.on("click", function(event){
+        event.preventDefault();
+        screens.fadeOut(1000);
+       // screens.css("")
+        startScreen.delay(1000).fadeIn(1000);
+        time.removeClass('finalCounting');        
+    });   
+    
+    
+    
+        /* AJAX 
     var tableScores = $('.tableScores'); //ul
     var scoreUrl = 'http://localhost:3000/game/';
     
@@ -315,5 +350,8 @@ $(function(){
         });
     }
     addResult();
+    */
+    
+
     
 });
